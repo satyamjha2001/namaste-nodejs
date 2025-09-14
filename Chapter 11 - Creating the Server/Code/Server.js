f3d66bb1433809a129f092eed8f51c20ffa95822
satyamjha2001 <satyamjha23112001@gmail.com>
2025-09-14 15:56:21 +0530

    update

--- Chapter 11 - Creating the Server/Code/Server.js
+++ Chapter 11 - Creating the Server/Code/Server.js
@@ -3,7 +3,7 @@ const http = require("node:http");
 const port = 999;
 const server = http.createServer(function (req, res) {
     if (req.url === "/getSecretData") {
-        res.end("You are a human and the the secret so chill")
+        res.end("You are a human and the secret is so chill")
     }
     res.end("server Created")
 })
